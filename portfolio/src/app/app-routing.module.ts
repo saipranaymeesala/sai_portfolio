1d7428a2c193d5d669731c39911cae4d437d0593
saipranaymeesala <meesala3438@gmail.com>
2024-11-11 12:43:24 +0530

    new data

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,20 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { MainComponent } from './main/main.component';
 
-const routes: Routes = [
-  {
-    path:'',component:MainComponent
-  },
-  {
-    path:'contact',component:ContactComponent
-  },
-  {
-    path:'**',component:NotfoundComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,20 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
-import { CertificationsComponent } from './certifications/certifications.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ContactComponent } from './contact/contact.component';
-import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    HeaderComponent,
-    CertificationsComponent,
-    ProjectsComponent,
-    ContactComponent,
-    NotfoundComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
